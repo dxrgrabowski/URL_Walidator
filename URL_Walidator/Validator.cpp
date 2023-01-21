b3632d1bc697a2081d84d30188e8af7e783619c9
FIRST_NAME LAST_NAME <mdexor@gmail.com>
2023-01-21 23:36:01 +0100

    Add Validator class methods

--- URL_Walidator/Validator.cpp
+++ URL_Walidator/Validator.cpp
@@ -1,6 +1,26 @@
 #pragma once
 #include "Validator.hpp"
 
-void Validator::validateObtainedData() {
+void Validator::validateObtainedData(vector<string>& addresses) {
+	auto it = addresses.begin();
 
+	while (it != addresses.end()) {
+		//methods here
+	}
+}
+
+void Validator::validateURLProtocol(string URL) {
+	size_t found=URL.find("://");
+	if (found != string::npos) {
+		string protocol = URL.substr(0, found);
+		if (protocol == "ftp" || protocol == "http" || protocol == "https") {
+
+		}
+		else {
+			//Output Handler WRONG PROTOCOL
+		}
+	}
+	else {
+		//Output Handler " :// " NOT FOUND 
+	}
 }
--- URL_Walidator/Validator.hpp
+++ URL_Walidator/Validator.hpp
@@ -1,9 +1,26 @@
 #pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
 
 class Validator {
 	
-	Validator() {};
+	vector<string> workAddresses;
+
+public:
+	Validator(vector<string> addresses) {
+		validateObtainedData(addresses);
+	}
 	~Validator()=default;
 
-	void validateObtainedData();
+	//Summarizing method
+	void isValidURL(vector<string>& addresses);
+	
+	//Checks if protocol and "://" is present and valid in URL adress http/https/ftp
+	void validateURLProtocol(string URL);
+	//Checks if domain is present and valid in URL adress 
+	void validateURLDomain();
+
+
 }
